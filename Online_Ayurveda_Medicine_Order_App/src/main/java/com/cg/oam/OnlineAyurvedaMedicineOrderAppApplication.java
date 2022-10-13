7bbf89b6ed74bc806edec324c7b58a9bd8133b73
Nikhil <nikhilmath99@gmail.com>
2022-10-13 18:21:54 +0530

    First Commit

---
+++ Online_Ayurveda_Medicine_Order_App/src/main/java/com/cg/oam/OnlineAyurvedaMedicineOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.cg.oam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAyurvedaMedicineOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAyurvedaMedicineOrderAppApplication.class, args);
+	}
+
+}
---
+++ Online_Ayurveda_Medicine_Order_App/src/test/java/com/cg/oam/OnlineAyurvedaMedicineOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.oam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAyurvedaMedicineOrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
