1f0c1f328662da700f918ba419374e664f8e18ed
Nikhil <nikhilmath99@gmail.com>
2022-10-15 10:57:47 +0530

    Adds API for user and admin

---
+++ Online_Ayurveda_Medicine_Order_App/src/main/java/com/cg/oam/controller/AdminAPI.java
@@ -0,0 +1,61 @@
+package com.cg.oam.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.oam.dto.AdminDTO;
+import com.cg.oam.exception.InvalidDataException;
+import com.cg.oam.service.IAdminService;
+
+
+@RestController
+@RequestMapping(value="/oam/administrator")
+public class AdminAPI {
+	
+	@Autowired
+	IAdminService adminService;
+	
+	@Autowired
+	Environment environment;
+	
+	@GetMapping(value = "/admins")
+	public ResponseEntity<List<AdminDTO>> getAllAdmins() throws InvalidDataException {
+		List<AdminDTO> admins = adminService.showAllAdmins();
+		return new ResponseEntity<>(admins, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/admin")
+	public ResponseEntity<String> addAdmin(@RequestBody AdminDTO admin) throws InvalidDataException{
+		adminService.addAdmin(admin);
+		String successMessage = environment.getProperty("API.INSERT_SUCCESS") + admin.getId();
+		return new ResponseEntity<>(successMessage, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(value = "/admin/{adminId}")
+	public ResponseEntity<String> updateAdmin(@PathVariable Integer adminId, @RequestBody AdminDTO admin)
+			throws InvalidDataException{
+		adminService.updateAdmin(admin);
+		String successMessage = environment.getProperty("API.UPDATE_SUCCESS");
+		return new ResponseEntity<>(successMessage, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/admin/{adminId}")
+	public ResponseEntity<String> removeAdmin(@PathVariable Integer adminId) throws InvalidDataException{
+		AdminDTO admin = new AdminDTO(Integer.toString(adminId),"TEST_VALUE");
+		adminService.removeAdmin(admin);
+		String successMessage = environment.getProperty("API.DELETE_SUCCESS");
+		return new ResponseEntity<>(successMessage, HttpStatus.OK);
+	}
+}
---
+++ Online_Ayurveda_Medicine_Order_App/src/main/java/com/cg/oam/controller/UserAPI.java
@@ -0,0 +1,62 @@
+package com.cg.oam.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.oam.dto.AdminDTO;
+import com.cg.oam.dto.UserDTO;
+import com.cg.oam.exception.InvalidDataException;
+import com.cg.oam.service.IAdminService;
+import com.cg.oam.service.IUserService;
+
+@RestController
+@RequestMapping(value="/oam/userinterface")
+public class UserAPI {
+	
+	@Autowired
+	IUserService userService;
+	
+	@Autowired
+	Environment environment;
+	
+	@GetMapping(value = "/users")
+	public ResponseEntity<List<UserDTO>> getAllAdmins() throws InvalidDataException {
+		List<UserDTO> users = userService.showAllUsers();
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/user")
+	public ResponseEntity<String> addAdmin(@RequestBody UserDTO user) throws InvalidDataException{
+		userService.addUser(user);
+		String successMessage = environment.getProperty("API.INSERT_SUCCESS") + user.getUserId();
+		return new ResponseEntity<>(successMessage, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(value = "/user/{userId}")
+	public ResponseEntity<String> updateAdmin(@PathVariable Integer userId, @RequestBody UserDTO user)
+			throws InvalidDataException{
+		userService.updateUser(user);
+		String successMessage = environment.getProperty("API.UPDATE_SUCCESS");
+		return new ResponseEntity<>(successMessage, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/user/{userId}")
+	public ResponseEntity<String> removeAdmin(@PathVariable Integer userId) throws InvalidDataException{
+		userService.removeUser(userId);
+		String successMessage = environment.getProperty("API.DELETE_SUCCESS");
+		return new ResponseEntity<>(successMessage, HttpStatus.OK);
+	}
+
+}
